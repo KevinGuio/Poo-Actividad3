38556cae6947f503b9c976ca25b988e5e9efb3b7
Kevin Guio <64978456+KevinGuio@users.noreply.github.com>
2022-10-01 16:55:35 -0500

    Add files via upload

---
+++ Cap3EjerResu7/src/cap3ejerresu7/Cap3EjerResu7.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cap3ejerresu7;
+
+public class Cap3EjerResu7 {
+
+    public static void main(String[] args) {
+        Screen scr = new Screen();
+        scr.setVisible(true);
+    }
+    
+}
---
+++ Cap4EjerProp22/src/cap4ejerprop22/Cap4EjerProp22.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cap4ejerprop22;
+
+public class Cap4EjerProp22 {
+
+    public static void main(String[] args) {
+        Screen scr = new Screen();
+        scr.setVisible(true);
+    }
+    
+}
