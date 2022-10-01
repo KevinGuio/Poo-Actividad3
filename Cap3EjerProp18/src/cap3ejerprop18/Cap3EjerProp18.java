4f3d522426c5853af9ba5508b1e2290d91e4db75
Kevin Guio <64978456+KevinGuio@users.noreply.github.com>
2022-10-01 16:52:54 -0500

    Add files via upload

---
+++ Cap3EjerProp18/src/cap3ejerprop18/Cap3EjerProp18.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cap3ejerprop18;
+
+public class Cap3EjerProp18 {
+
+    public static void main(String[] args) {
+        Screen scr = new Screen();
+        scr.setVisible(true);
+    }
+    
+}
